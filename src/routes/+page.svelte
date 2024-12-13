daaff01fcb03da680b742a25999150bd1f918ea6
jamesbeadle <94673648+jamesbeadle@users.noreply.github.com>
2024-12-13 10:03:08 +0000

    Update google login flow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,38 @@
 <script lang="ts">
     import Layout from "./Layout.svelte";  
+    import Google from "$lib/components/icons/google.svelte";
+    import { createClient } from '@supabase/supabase-js';
+  
+    const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+  
+    async function signInWithGoogle() {
+      const { data, error: authError } = await supabase.auth.signInWithOAuth({
+        provider: 'google'
+      });
+  
+      if (authError) {
+        console.error('Error signing in with Google:', authError.message);
+      } else {
+
+        //TODO Set Store variables
+
+        console.log(data);
+      }
+    }
+  
 </script>
 <Layout>    
    
     <div class="z-10 px-4 mb-20 text-center">
         <h1 class="mb-1 font-bold text-BrandForest">WELCOME TO <span class="condensed">GOLFPAD</span></h1>
         <h2 class="mx-16 mb-6 text-5xl font-black leading-tight text-black md:text-6xl condensed">THE FUTURE OF GOLF STARTS HERE</h2>
-        <a href="/sign-in"><button class="brand-button">LOGIN</button></a>
+        
+        <button on:click={signInWithGoogle} class="brand-button">
+            <span class="flex flex-row items-center">
+                Sign In With Google
+                <Google className="ml-2 w-6" />
+            </span>
+        </button>
     </div>
     <div class="absolute bottom-0 left-0 z-0 w-full">
         <img src="golfball_mobile.png" alt="Golf Ball" class="object-cover w-full h-auto md:hidden">
