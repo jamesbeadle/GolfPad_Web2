541bb9ce8872ae381a55132f63072b9f28abe255
jamesbeadle <94673648+jamesbeadle@users.noreply.github.com>
2024-12-13 21:33:45 +0000

    remove cookie block in hook server

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,12 +7,7 @@ export const handle: Handle = async ({ event, resolve }) => {
   event.locals.supabase = createSupabaseServerClient({
     supabaseUrl: PUBLIC_SUPABASE_URL,
     supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-    event,
-    cookieOptions: {
-      domain: 'www.golfpad.xyz',  // match your domain exactly
-      sameSite: 'none',
-      secure: true
-    }
+    event
   });
 
   const { data: { session } } = await event.locals.supabase.auth.getSession();
