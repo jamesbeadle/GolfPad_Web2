1dd4e4574b525772b48723eb3d7703e7eeb50cb0
jamesbeadle <94673648+jamesbeadle@users.noreply.github.com>
2024-12-13 00:03:03 +0000

    Add login redirect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     <div class="z-10 px-4 mb-20 text-center">
         <h1 class="mb-1 font-bold text-BrandForest">WELCOME TO <span class="condensed">GOLFPAD</span></h1>
         <h2 class="mx-16 mb-6 text-5xl font-black leading-tight text-black md:text-6xl condensed">THE FUTURE OF GOLF STARTS HERE</h2>
-        <button class="brand-button">LOGIN</button>
+        <a href="/sign-in"><button class="brand-button">LOGIN</button></a>
     </div>
     <div class="absolute bottom-0 left-0 z-0 w-full">
         <img src="golfball_mobile.png" alt="Golf Ball" class="object-cover w-full h-auto md:hidden">
