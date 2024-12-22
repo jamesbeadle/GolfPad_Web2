bc242448a75afeb72e2b956c3b2d5321232a2ef2
jamesbeadle <94673648+jamesbeadle@users.noreply.github.com>
2024-12-22 08:29:59 +0000

    Update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,17 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   preprocess: vitePreprocess(),
-
   kit: {
     adapter: adapter({
-      out: 'build',
-      // This ensures the built server reads `PORT` from environment
       env: {
         host: '0.0.0.0',
-
-
-                port: process.env.PORT || 8080
-
-
+        port: process.env.PORT || 8080
       }
     })
   }
 };
+
+export default config;
